b54826d865643d5be860ae2315253d65b2c2855d
Alina Rimanas <alina.rimanas@gmail.com>
2024-04-12 02:51:27 +0300

    init state _create project

---
+++ AQA_MTS_Graduate_Work/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AQA_MTS_Graduate_Work
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
